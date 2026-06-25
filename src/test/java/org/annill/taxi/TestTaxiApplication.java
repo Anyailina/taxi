d47b54e669fb56d052464a329a2a767d5cf49fce
Ann <annill13@mail.ru>
2026-06-25 20:25:10 +0300

    initial commit

---
+++ src/test/java/org/annill/taxi/TestTaxiApplication.java
@@ -0,0 +1,11 @@
+package org.annill.taxi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TaxiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
